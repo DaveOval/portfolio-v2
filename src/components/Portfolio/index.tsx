035b53ec4f12823b229476918b5d068f7b230347
David Vazquez <dave_u@outlook.com>
2023-07-11 22:41:49 -0600

    archivo .htaccess agregado

--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import { colorsui } from '../../ui/colors';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 
 const PortFolioContainer = styled.section`
@@ -41,20 +40,7 @@ const SubTitle = styled(Link)`
         color: #999999;
     }
 `
-const Button = styled.button`
-    width: 2rem;
-    height: 2rem;
-    border-radius: 50%;
-    border: none;
-    background-color: ${colorsui.primaryColor};
-    color: ${colorsui.backGroundGray};
-    font-size: 1.5rem;
-    cursor: pointer;
-    transition: all 0.3s ease-in-out;
-    &:hover{
-        background-color: ${colorsui.secondaryColor};
-    }
-`
+
 const Container = styled.div`
     width: 100%;
     display: flex;
@@ -63,41 +49,20 @@ const Container = styled.div`
     align-items: center;
     flex-wrap: wrap;
 `
-const CardsContainer = styled.div`
-    width: 90%;
-    height: auto;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    /* flex-wrap: wrap; */
-    /* overflow-x: scroll; */
-`
-const Card = styled.div`
-    width: 345px;
-    height: 300px;
-    margin: 1rem;
-    border-radius: 15px;
-    overflow: hidden;
-    img{
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-`
+
 
 
 export const FrontPortFolio = () => {
 
-    const [currentCardIndex, setCurrentCardIndex] = useState(0);
-    const [visibleCards, setVisibleCards] = useState(3);
+    /* const [currentCardIndex, setCurrentCardIndex] = useState(0); */
+    /* const [visibleCards, setVisibleCards] = useState(3); */
 
-    const previousCards = () => {
+    /* const previousCards = () => {
         setCurrentCardIndex( prevIndex => prevIndex - visibleCards )
     }
     const nextCards = () => {
         setCurrentCardIndex( prevIndex => prevIndex + visibleCards )
-    }
+    } */
 
     return (
         <>
@@ -106,7 +71,7 @@ export const FrontPortFolio = () => {
                     <Title>Front-end Portolio</Title>
                     <SubTitle to="/projects">See All</SubTitle>
                     <Container>
-                        <Button onClick={previousCards} disabled={currentCardIndex === 0 }>{"<"}</Button>
+                        {/* <Button onClick={previousCards} disabled={currentCardIndex === 0 }>{"<"}</Button>
                         <CardsContainer>
                             <Card>
                                 <img src="https://picsum.photos/200/300" alt="img" />
@@ -148,7 +113,7 @@ export const FrontPortFolio = () => {
                                 <img src="https://picsum.photos/200/300" alt="img" />
                             </Card>
                         </CardsContainer>
-                        <Button onClick={nextCards} disabled={currentCardIndex >= 60}>{">"}</Button>
+                        <Button onClick={nextCards} disabled={currentCardIndex >= 60}>{">"}</Button> */}
                     </Container>
                 </Header>
             </PortFolioContainer>
